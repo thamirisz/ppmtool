8c297a1d26bc2a9b1d551c8249a38e48434fd414
thamirisz <thamiris.zhang.dev@gmail.com>
2021-11-15 22:40:48 -0800

    Initial Commit

---
+++ src/test/java/dev/tz/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tz.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
